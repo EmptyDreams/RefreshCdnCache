7b573a76f77cbf9d62210c387a8cde95fb27e592
kmar <minedreams@qq.com>
2024-12-01 20:14:02 +0800

    移除华为云和阿里云的支持

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,5 @@
-import {AliOptional, invokeAli} from './ali'
-import {HuaweiOptional, invokeHuawei} from './huawei'
 import {UpyunOptional, invokeUpyun} from './upyun'
 import {DogeCloudOptional, invokeDogeCloud} from './doge-cloud'
-
-export function refreshAli(optional: AliOptional) {
-    optional.type = 'ali'
-    return invokeAli(optional)
-}
-
-export function refreshHuawei(optional: HuaweiOptional) {
-    optional.type = 'huawei'
-    return invokeHuawei(optional)
-}
 
 export function refreshUpyun(optional: UpyunOptional) {
     optional.type = 'upyun'
